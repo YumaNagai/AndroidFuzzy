3233e5f33b19c9cb81f0b38d057eb11340bb0f0f
YumaNagai <yumaengineer@gmail.com>
2014-11-21 20:40:42 +0900

    second commit change interface source code

--- src/com/fuzzymeasure/MyFuzzyMeasure.java
+++ src/com/fuzzymeasure/MyFuzzyMeasure.java
@@ -1,16 +1,12 @@
 package com.fuzzymeasure;
 
-import java.io.*;
-import java.util.Vector;
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.os.*;
 import android.view.View;
 import android.view.Window;
 import android.widget.*;
-import com.neurosky.thinkgear.*;
 
 public class MyFuzzyMeasure extends Activity 
 {
@@ -52,15 +48,13 @@ public class MyFuzzyMeasure extends Activity
     {
         super.onCreate(bundle);
         thinkgearDevice = MyThinkGear.Load();		//create&init thinkGearDevice
-        InitializeContent();
-        
-        
+        InitializeComponent();
     }
 	
 
     
     //content Initialize
-	private void InitializeContent()
+	private void InitializeComponent()
 	{	
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
@@ -75,45 +69,30 @@ public class MyFuzzyMeasure extends Activity
 		devicestatus.setTextSize(25.0f);
 		devicetable.addView(devicestatus);
 		
-		connetbutton = new Button(this);
-		connetbutton.setText("connectDevice");
-		connetbutton.setOnClickListener(new View.OnClickListener() 
+		connetButton = new Button(this);
+		connetButton.setText("connectDevice");
+		connetButton.setOnClickListener(new View.OnClickListener() 
 		{			
 			@Override
 			public void onClick(View v) 
 			{
 				// TODO Auto-generated method stub
-				if(tgDevice.getState() != TGDevice.STATE_CONNECTING && tgDevice.getState() != TGDevice.STATE_CONNECTED)
-				{
-		    		tgDevice.connect(rawEnabled);
-		    		//tgDevice.start();
-				}
+				
 			}
 		} );
-		devicetable.addView(connetbutton);
+		devicetable.addView(connetButton);
 		
-		disconnectbutton = new Button(this);
-		disconnectbutton.setText("DisconnectDevice");
-		disconnectbutton.setOnClickListener(new View.OnClickListener() 
+		disconnectButton = new Button(this);
+		disconnectButton.setText("DisconnectDevice");
+		disconnectButton.setOnClickListener(new View.OnClickListener() 
 		{
 			@Override
 			public void onClick(View v) 
 			{
-				// TODO Auto-generated method stub
-				if(tgDevice.getState() == TGDevice.STATE_CONNECTING && tgDevice.getState() == TGDevice.STATE_CONNECTED)
-				{
-		    		tgDevice.close();
-		    		runOnUiThread(new Runnable() 
-					{
-						public void run() 
-						{
-							statuslog.append("TGDeviceDisconnect");
-						}
-					});
-				}
+				
 			}
 		});
-		devicetable.addView(disconnectbutton);
+		devicetable.addView(disconnectButton);
 		layout.addView(devicetable);
 		layout.addView(new TextView(this));
 		//------------------------------------------------------------------
@@ -185,42 +164,20 @@ public class MyFuzzyMeasure extends Activity
 			@Override
 			public void onClick(View v) 
 			{
-				if(issetting == true && isexpriment == false && tgDevice.getState() == TGDevice.STATE_CONNECTED)
-				{
-					// TODO Auto-generated method stub
-					gender = (genderType.getSelectedItemPosition() == 0 ) ? "男" : "女";
-					age = Integer.parseInt(ageText.getText().toString());
-					expriment = (experimentType.getSelectedItemPosition() == 0 ) ? "Yes" : "No";
-					measuretime = Integer.parseInt(secondText.getText().toString());
-					sampleCount = sampingRate * measuretime;
-					if(sampleCount == 0)
-					{
-						statuslog.append("Can't measure 0 times\n");
-						return;
-					}
-					wavedata = new double[sampleCount];
-					nowmeasurecount = 0;
-					statuslabel.setText("MeasureBrainWave");
-					statuslog.setText("");
-					tgDevice.start();
-					isexpriment = true;
-					isresultfinish = false;
-					issetting = false;
-					mstart = timestmp = System.currentTimeMillis();
-				}
-				
 			}
 		});
 		buttonsTable.addView(expStartButton);
 		
 		analyzeButton = new Button(this);
 		analyzeButton.setText("AnalyzeData");
+		analyzeButton.setEnabled(true);
 		analyzeButton.setOnClickListener(new View.OnClickListener() 
 		{
 			
 			@Override
 			public void onClick(View v) 
 			{
+				/*
 				// TODO Auto-generated method stub
 		    	//実験終了時の処理
 		    	if(issetting == false && isexpriment == false && isresultfinish == false)
@@ -338,7 +295,7 @@ public class MyFuzzyMeasure extends Activity
 		    	{
 		    		statuslog.append("Now DataSetting\n");
 		    	}
-			}
+			*/}
 		});
 		buttonsTable.addView(analyzeButton);
 		
@@ -398,109 +355,11 @@ public class MyFuzzyMeasure extends Activity
 		
 	}
     
-    
-        
-    //public void run()
-    //{	
-    	/*
-    	//実験終了時の処理
-    	if(issetting == false && isexpriment == false)
-    	{
-    		//tgDevice stop
-    		//tgDevice.close();
-    		    		
-    		//FFTデータ作成
-    		datafunction = MyFFTFunction.Load(sampleCount, wavedata,MyFFTFunction.WindowType.Hamming);
-    		
-    		double datap[] = new double[5];
-    		
-    		datap[0] = datafunction.getWaveProbability(MyFFTFunction.WaveBandType.Delta) * 100;
-    		datap[1] = datafunction.getWaveProbability(MyFFTFunction.WaveBandType.Theta) * 100;
-    		datap[2] = datafunction.getWaveProbability(MyFFTFunction.WaveBandType.Alpha) * 100;
-    		datap[3] = datafunction.getWaveProbability(MyFFTFunction.WaveBandType.Beta)  * 100;
-    		datap[4] = datafunction.getWaveProbability(MyFFTFunction.WaveBandType.Gamma) * 100;
-    		
-    		double Ngrade = 0,Ntgrade = 0;
-    		double Ygrade = 0,Ytgrade = 0;
-    		
-    		int tabelsize = ruletable.size();
-    		
-    		//ルールテール分繰り返し
-    		for(int ii=0;ii<tabelsize;ii++)
-    		{
-    			double min = Double.MAX_VALUE;
-    			FuzzyRlue rule = (FuzzyRlue)ruletable.get(ii);
-    			char state = '0';
-    			
-    			for(int jj=0;jj<5;jj++)
-    			{
-    				switch(jj)
-    				{
-    					case 0: state = rule.deltaState;  break;
-    					case 1: state = rule.thetaState;  break;
-    					case 2: state = rule.alphaState;  break;
-    					case 3: state = rule.betaState;   break;
-    					case 4: state = rule.gammaState;  break;
-    				}
-    				double pp = membersipdata[jj].fuzzyset(datap[jj],state);
-    				if(min >= pp) min = pp;	
-    			}
-    			
-    			//Yes出力
-				Ygrade += min;
-				Ytgrade += (min * rule.yesWeight);
-										
-				//No出力
-				Ngrade += min;
-				Ntgrade += (min * rule.noWeight);
-    		}
-    		Ytgrade = (Ygrade == 0) ? 0 : Ytgrade/Ygrade;
-			Ntgrade = (Ngrade == 0) ? 0 : Ntgrade/Ngrade;
-			
-			final String delta = "delta:"+ String.format("%.2f",(datap[0])) +"%\n";
-			final String theta = "theta:"+ String.format("%.2f",(datap[1])) +"%\n";
-			final String alpha = "alpha:"+ String.format("%.2f",(datap[2])) +"%\n";
-			final String beta =  "beta:" + String.format("%.2f",(datap[3])) +"%\n";
-			final String gamma = "gamma:"+ String.format("%.2f",(datap[4])) +"%\n\n";
-			final String Yesw =  "Yes: "+  String.format("%.2f",(Ytgrade * 100)) +"%\n" ;
-			final String Now =   "No:  " + String.format("%.2f",(Ntgrade * 100)) +"%\n";
-			
-			this.runOnUiThread(new Runnable() 
-			{
-				@Override
-				public void run() 
-				{
-					// TODO Auto-generated method stub
-					//結果表示
-					
-					statuslabel.setText("");
-					statuslabel.append("MeasureResult");
-					statuslog.setText("");
-					statuslog.append(delta);
-					statuslog.append(theta);
-					statuslog.append(alpha);
-					statuslog.append(beta);
-					statuslog.append(gamma);
-					statuslog.append(Yesw);
-					statuslog.append(Now);
-				}
-			});
-			//tgDevice.
-			
-			nowmeasurecount = 0;
-			isexpriment = true;
-			issetting = true;
-			isresultfinish = true;
-			//mainRoutine.stop();
-    	}
-    	*/
-    //}
-    
     @Override
     //call Activity destroy
     public void onDestroy() 
     {
-    	tgDevice.close();
+    	
         super.onDestroy();
     }
 
